6dbd24450abe483f73def2aa42cdb9a74cf048d3
Amelie Oller <amelieoller@gmail.com>
2019-11-28 11:23:06 -0800

    Update footer with expand functionality

--- app/components/Projects/Project/Footer.js
+++ app/components/Projects/Project/Footer.js
@@ -7,23 +7,33 @@ import { ReactComponent as Edit } from '../../../assets/icons/edit.svg';
 import { ReactComponent as GitHub } from '../../../assets/icons/github.svg';
 import { ReactComponent as Monitor } from '../../../assets/icons/monitor.svg';
 import { ReactComponent as FileText } from '../../../assets/icons/file-text.svg';
-import { ReactComponent as Server } from '../../../assets/icons/server.svg';
+import { ReactComponent as Code } from '../../../assets/icons/code.svg';
+import { ReactComponent as ChevronDown } from '../../../assets/icons/chevron-down.svg';
+import { ReactComponent as ChevronUp } from '../../../assets/icons/chevron-up.svg';
 
 const StyledFooter = styled.div`
   text-align: right;
-  margin: 1rem 3.5rem 3.5rem 3.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 2rem;
 
   a {
     color: inherit;
     margin-left: 0.8rem;
+
+    svg {
+      margin-left: 0.4rem;
+    }
   }
 
   svg {
-    margin-left: 0.5rem;
     cursor: pointer;
+    color: ${props => props.theme.lightGrey};
 
     &:hover {
-      color: ${props => props.theme.primary};
+      transition: all ${props => props.theme.transitions.ease};
+      color: ${props => props.theme.darkerGrey};
     }
   }
 
@@ -33,42 +43,49 @@ const StyledFooter = styled.div`
 `;
 
 const Footer = ({
-  project: { github, live, server, folder, id },
-  showNotesDiv,
-  openServer,
-  handleDelete
+  project: { github, live, server, folder, id, additionalImage },
+  openFolderInEditor,
+  handleDelete,
+  expandCard,
+  extraInfo
 }) => (
-  <StyledFooter>
-    {github && (
-      <a target="_blank" rel="noopener noreferrer" href={github}>
-        <GitHub />
-      </a>
+  <StyledFooter className="footer">
+    {folder || additionalImage ? (
+      extraInfo ? (
+        <ChevronUp onClick={expandCard} />
+      ) : (
+        <ChevronDown onClick={expandCard} />
+      )
+    ) : (
+      <span></span>
     )}
-    {server && (
-      <a target="_blank" rel="noopener noreferrer" href={live}>
-        <Monitor />
+    <div>
+      {github && (
+        <a target="_blank" rel="noopener noreferrer" href={github}>
+          <GitHub />
+        </a>
+      )}
+      {server && (
+        <a target="_blank" rel="noopener noreferrer" href={live}>
+          <Monitor />
+        </a>
+      )}
+      <Link to={`/projects/${id}/edit`}>
+        <Edit />
+      </Link>
+      {server && (
+        <a onClick={openFolderInEditor}>
+          <Code />
+        </a>
+      )}
+      <a
+        onClick={() => {
+          if (window.confirm('Are you sure?')) handleDelete(id);
+        }}
+      >
+        <Trash />
       </a>
-    )}
-    <Link to={`/projects/${id}/edit`}>
-      <Edit />
-    </Link>
-    {folder && (
-      <a onClick={showNotesDiv}>
-        <FileText />
-      </a>
-    )}
-    {server && (
-      <a onClick={openServer}>
-        <Server />
-      </a>
-    )}
-    <a
-      onClick={() => {
-        if (window.confirm('Are you sure?')) handleDelete(id);
-      }}
-    >
-      <Trash />
-    </a>
+    </div>
   </StyledFooter>
 );
 
