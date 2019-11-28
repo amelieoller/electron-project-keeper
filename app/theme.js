6ecb33e5e8f024adc02d355073b3149a2d0ed911
Amelie Oller <amelieoller@gmail.com>
2019-11-28 11:20:25 -0800

    Add colors, sizes and transitions to theme

--- app/theme.js
+++ app/theme.js
@@ -4,7 +4,20 @@ const theme = {
   primaryDark: '#675dfc',
   primaryLight: '#CAC6FF',
   primaryBackground: '#ff9a9a',
-  text: '#50514f'
+  text: '#50514f',
+  lightGrey: '#E3E8EB',
+  lighterGrey: '#E9EFF4',
+  lightestGrey: '#F4F7F9',
+  darkerGrey: '#989898',
+  border: ' 1px solid #D0D7DD',
+
+  sizes: {
+    borderRadius: '3px'
+  },
+
+  transitions: {
+    ease: '0.2s ease'
+  }
 };
 
 export default theme;
