08a94b92808aa77875955c5e43ee54a6bd036b41
Amelie Oller <amelieoller@gmail.com>
2019-11-29 08:45:37 -0800

    Update input component and include textarea

--- app/components/Projects/Form/Input/Input.js
+++ app/components/Projects/Form/Input/Input.js
@@ -6,53 +6,65 @@ const StyledInput = styled.div`
   margin-bottom: 1.6rem;
 
   label {
-    font-size: 1.8rem;
+    font-size: 1.2rem;
     font-weight: 300;
-    color: #818181;
+    color: ${props => props.theme.grey};
+    text-transform: uppercase;
   }
 
-  input {
+  textarea {
+    height: 8rem;
+    resize: none;
+  }
+
+  input,
+  textarea {
     margin-top: 0.4rem;
-    font-size: 1.4rem;
+    font-size: 1.6rem;
     width: 100%;
-    padding: 0.4rem 0.8rem;
-    border: 1px solid #c5c5c5;
-    color: ${props => props.theme.text};
+    padding: 1.4rem 1.8rem;
+    color: ${props => props.theme.textDark};
+    border: ${props => props.theme.border};
+    border-radius: ${props => props.theme.sizes.borderRadius};
+
+    &::placeholder {
+      color: ${props => props.theme.darkerGrey};
+    }
   }
 
-  input:focus {
+  input:focus,
+  textarea:focus {
     border-color: ${props => props.theme.primary}87;
-    box-shadow: 0 1px 1px rgba(229, 103, 23, 0.075) inset, 0 0 8px ${props => props.theme.primary}87;
+    box-shadow: 0 1px 1px rgba(229, 103, 23, 0.075) inset,
+      0 0 8px ${props => props.theme.primary}87;
     outline: 0 none;
   }
 `;
 
-const Input = props => {
-  return (
-    <StyledInput>
-      <label htmlFor={props.name} className="form-label">
-        {props.title}
-      </label>
-      <input
-        className="form-input"
-        id={props.name}
-        name={props.name}
-        type={props.type}
-        value={props.value}
-        onChange={props.handleChange}
-        placeholder={props.placeholder}
-      />
-    </StyledInput>
-  );
-};
+const Input = props => (
+  <StyledInput textarea={props.type}>
+    <label htmlFor={props.name}>{props.title}</label>
+    {props.type === 'textarea' ? (
+      <textarea id={props.name} {...props} />
+    ) : (
+      <input id={props.name} {...props} />
+    )}
+  </StyledInput>
+);
 
 Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
   placeholder: PropTypes.string.isRequired,
-  required: PropTypes.bool
+  title: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  required: PropTypes.bool,
+  type: PropTypes.string
 };
 
 Input.defaultProps = {
-  required: false
+  required: false,
+  type: 'text'
 };
 
 export default Input;
