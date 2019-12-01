dd4562ac3aacc14726a99f15d9cba69f47d0e826
Amelie Oller <amelieoller@gmail.com>
2019-12-01 10:13:29 -0800

    Remove unused files

--- app/reducers/types.js
+++ app/reducers/types.js
@@ -1,15 +1,9 @@
 import type { Dispatch as ReduxDispatch, Store as ReduxStore } from 'redux';
 
-export type counterStateType = {
-  +counter: number
-};
-
 export type Action = {
   +type: string
 };
 
-export type GetState = () => counterStateType;
-
 export type Dispatch = ReduxDispatch<Action>;
 
-export type Store = ReduxStore<GetState, Action>;
+export type Store = ReduxStore<Action>;
