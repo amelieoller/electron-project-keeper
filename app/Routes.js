83628a959adca3ffde88eef853b56aae4f001e59
Amelie Oller <amelieoller@gmail.com>
2019-12-21 17:21:40 -0800

    Change routes based on logged in user

--- app/Routes.js
+++ app/Routes.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { Switch, Route } from 'react-router';
+import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import routes from './constants/routes';
 import App from './containers/App';
 import Home from './containers/Home';
 import NewProject from './components/NewProject';
 import EditProject from './components/EditProject';
+import withUser from './components/withUser';
+import Login from './components/Login';
 
-export default () => (
-  <App>
-    <Switch>
-      <Route path={routes.HOME} exact component={Home} />
-      <Route path={routes.NEW_PROJECT} component={NewProject} />
-      <Route path={routes.EDIT_PROJECT} exact component={EditProject} />
-    </Switch>
-  </App>
-);
+class Routes extends Component {
+  render() {
+    const { user } = this.props;
+
+    return (
+      <App>
+        {user ? (
+          <Switch>
+            <Route path={routes.NEW_PROJECT} component={NewProject} />
+            <Route path={routes.EDIT_PROJECT} exact component={EditProject} />
+            <Route path={routes.HOME} component={Home} />
+            <Redirect to={routes.HOME} />
+          </Switch>
+        ) : (
+          <Switch>
+            <Route exact path={routes.HOME} component={Login} />
+            <Redirect to={routes.HOME} />
+          </Switch>
+        )}
+      </App>
+    );
+  }
+}
+
+export default withUser(Routes);
