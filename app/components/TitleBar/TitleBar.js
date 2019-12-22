04afc4a1f4827b474a7f9a37e15ed62973ca8626
Amelie Oller <amelieoller@gmail.com>
2019-12-21 17:19:09 -0800

    Update titlebar styles

--- app/components/TitleBar/TitleBar.js
+++ app/components/TitleBar/TitleBar.js
@@ -3,57 +3,60 @@ import styled from 'styled-components';
 
 const StyledTitleBar = styled.div`
   color: ${({ theme }) => theme.darkerGrey};
-  height: 2.3rem;
+  height: 5rem;
   text-align: center;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   z-index: 1;
-
+  font-size: 0.95rem;
+  background: white;
+  
   .right {
-    position: absolute;
-    right: 1.7rem;
-    top: 1.7rem;
+    display: flex;
+    float: right;
+    padding: 1rem;
 
     a {
       display: flex;
       flex-direction: column;
       text-decoration: none;
-      color: ${({ theme }) => theme.primary};
+      color: ${({ theme }) => theme.primaryLight};
+      width: 5.5rem;
+      cursor: pointer;
+      align-items: center;
 
       svg {
-        height: 2.3rem;
+        height: 2rem;
         margin-bottom: 0.2rem;
-
-        path {
-          fill: ${({ theme }) => theme.primary};
-        }
       }
 
       &:hover {
-        color: ${({ theme }) => theme.primaryLight};
-
-        path {
-          fill: ${({ theme }) => theme.primaryLight};
-        }
+        color: ${({ theme }) => theme.primary};
       }
     }
+  }
 
-    @media (max-width: 550px) {
+  @media (max-width: 550px) {
+    color: white;
+
+    .right {
       right: 0.5rem;
-      top: 0.5rem;
+      top: 0.8rem;
 
-      svg {
-        height: 1.4rem;
+      a {
+        color: white;
 
-        path {
-          fill: white;
+        svg {
+          color: white;
         }
 
         &:hover {
-          path {
-            fill: ${({ theme }) => theme.primaryDark};
+          color: ${({ theme }) => theme.primaryDark};
+
+          svg {
+            color: ${({ theme }) => theme.primaryDark};
           }
         }
       }
@@ -66,12 +69,10 @@ const StyledTitleBar = styled.div`
   }
 `;
 
-const TitleBar = ({ children }) => {
-  return (
-    <StyledTitleBar>
-      <div className="right">{children}</div>
-    </StyledTitleBar>
-  );
-};
+const TitleBar = ({ children }) => (
+  <StyledTitleBar>
+    <div className="right">{React.Children.map(children, (child, i) => child)}</div>
+  </StyledTitleBar>
+);
 
 export default TitleBar;
