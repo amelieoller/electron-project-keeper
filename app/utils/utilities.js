60ebeb007df1d7608442af23ae763c85897e7ffb
Amelie Oller <amelieoller@gmail.com>
2019-12-23 13:48:54 -0800

    Create moveArrayItem util

--- app/utils/utilities.js
+++ app/utils/utilities.js
@@ -21,3 +21,16 @@ export const twoFlatArraysAreEqual = (array1, array2) => {
 
   return true;
 };
+
+export const moveArrayItem = (arr, old_index, new_index) => {
+  if (new_index >= arr.length) {
+    var k = new_index - arr.length + 1;
+    while (k--) {
+      arr.push(undefined);
+    }
+  }
+
+  arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+
+  return arr;
+};
